184be5cc2fce7332bd7f53f9e03b73c0eb2198fb
Abyan <harlem.minion09@gmail.com>
2019-10-05 11:40:51 +0700

    Penambahan anim pada menu pesan

--- app/src/main/java/com/abyan/pesenmaem/activities/MenuPesanActivity.java
+++ app/src/main/java/com/abyan/pesenmaem/activities/MenuPesanActivity.java
@@ -1,11 +1,14 @@
 package com.abyan.pesenmaem.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 
 import com.abyan.pesenmaem.R;
+import com.abyan.pesenmaem.fragments.MakananFragment;
+import com.abyan.pesenmaem.fragments.MinumanFragment;
 
 public class MenuPesanActivity extends AppCompatActivity {
 
@@ -16,8 +19,30 @@ public class MenuPesanActivity extends AppCompatActivity {
     }
 
     public void handlerClickMakananFragment(View view) {
+        FragmentTransaction fragmentTransaction =getSupportFragmentManager().beginTransaction();
+
+        MakananFragment makananFragment = (MakananFragment) getSupportFragmentManager().findFragmentByTag("MAKANAAN_FRAGMENT");
+        if(makananFragment != null && makananFragment.isVisible()){
+            fragmentTransaction.commit();
+        }else {
+            fragmentTransaction.setCustomAnimations(R.anim.exit_from_right, R.anim.exit_from_left);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder, new MakananFragment(), "MAKANAAN_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 
     public void handlerClickMinumanFragment(View view) {
+        FragmentTransaction fragmentTransaction =getSupportFragmentManager().beginTransaction();
+
+        MinumanFragment minumanFragment = (MinumanFragment) getSupportFragmentManager().findFragmentByTag("MINUMAN_FRAGMENT");
+        if(minumanFragment != null && minumanFragment.isVisible()){
+            fragmentTransaction.commit();
+        }else {
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.enter_from_right);
+            fragmentTransaction.replace(R.id.dynamic_fragment_placeholder, new MinumanFragment(), "MINUMAN_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 }
