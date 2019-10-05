9865e1ad86c3d066bb3600440a9e666e95de4651
Abyan <harlem.minion09@gmail.com>
2019-10-05 11:16:01 +0700

    Templetnya

---
+++ app/src/main/java/com/abyan/pesenmaem/activities/MenuAwalActivity.java
@@ -0,0 +1,20 @@
+package com.abyan.pesenmaem.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.abyan.pesenmaem.R;
+
+public class MenuAwalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_awal);
+    }
+
+    public void handlerClickMenuFragment(View view) {
+    }
+}
---
+++ app/src/main/java/com/abyan/pesenmaem/activities/MenuPesanActivity.java
@@ -0,0 +1,23 @@
+package com.abyan.pesenmaem.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.abyan.pesenmaem.R;
+
+public class MenuPesanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_pesan);
+    }
+
+    public void handlerClickMakananFragment(View view) {
+    }
+
+    public void handlerClickMinumanFragment(View view) {
+    }
+}
