8517a87e9613daaae5d28d4ec3f1a16fa4eb88c6
Abyan <harlem.minion09@gmail.com>
2019-10-10 10:50:59 +0700

    memperbaiki Tampilan

--- app/src/main/java/com/abyan/pesenmaem/MainActivity.java
+++ app/src/main/java/com/abyan/pesenmaem/MainActivity.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.abyan.pesenmaem.activities.MenuAwalActivity;
+import com.abyan.pesenmaem.fragments.AboutFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AboutFragment aboutFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aboutFragment = AboutFragment.newInstance("Moch. Abyan An-Nambhany");
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, aboutFragment)
+                .addToBackStack(null)
+                .commit();
+
+
     }
 
     public void handlerClickMenuAwalFragment(View view) {
